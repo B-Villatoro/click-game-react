deeb338c49b7fc38c9d08045d17087310480e43a
Bryan Villatoro <bvillatoro@ymail.com>
2019-05-08 09:29:07 -0400

    character card set up

--- click-game/src/App.js
+++ click-game/src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import CharCard from "./components/charCard";
+import Characters from "./characters.json";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {Characters.map((item)=>{
+        return (
+        <CharCard
+          key={item.id}
+          name={item.name}
+          image={item.image}
+        />
+      )
+      })}
+
+
     </div>
   );
 }
--- click-game/src/components/charCard/index.js
+++ click-game/src/components/charCard/index.js
@@ -3,25 +3,12 @@ import React from "react";
 
 function CharacterCard(props) {
   return (
-    <div className="card">
+    <div className="card" key={props.id}>
       <div className="img-container">
-        <img alt={props.name} src={props.image} />
-      </div>
-      <div className="content">
-        <ul>
-          <li>
-            <strong>Name:</strong> {props.name}
-          </li>
-          <li>
-            <strong>Occupation:</strong> {props.occupation}
-          </li>
-          <li>
-            <strong>Location:</strong> {props.location}
-          </li>
-        </ul>
+        <img alt={props.name} src={props.image} width="200" height="200"/>
       </div>
     </div>
   );
 }
 
-export default FriendCard;
+export default CharacterCard;
